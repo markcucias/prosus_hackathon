e71708a55028bb4d0984e5aa64535f3751a3d6d4
Marc Cucias <kuchiyashm@gmail.com>
2025-11-08 19:55:38 +0100

    removed unnecessary web repo

--- web/src/components/Chip.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Chip({ children, color = 'gray' }: { children: React.ReactNode, color?: string }){
-  const bg = `bg-${color}-100`
-  const text = `text-${color}-800`
-  return (
-    <span className={`inline-flex items-center px-2 py-0.5 rounded text-sm ${bg} ${text}`}>{children}</span>
-  )
-}
