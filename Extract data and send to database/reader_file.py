3bcd7a1d9b4cb9cdc33f32776106bb73a1aa054e
Patrino92 <56628287+Patrino92@users.noreply.github.com>
2025-11-08 16:09:27 +0100

    Uploading to database

--- Extract data and send to database/reader_file.py
+++ Extract data and send to database/reader_file.py
@@ -7,7 +7,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-from database import insert_event  # import MongoDB helper
+from database2 import insert_event  # import MongoDB helper
 
 # Permission to read calendar data
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
